494821a4caba4c758e20211d7bca553613f9fcc5
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-15 10:56:02 +0200

    Added div appenchild style

--- index.js
+++ index.js
@@ -4,15 +4,8 @@ const button = document.querySelector(".button");
 const text = document.querySelector(".button").value;
 const div = document.querySelector("div");
 
-button.addEventListener("click", 
-() => {
-  const receivedTag = document.querySelector(".main-input").value;
-  
-  const newComponent = Component(text.value));
-
-
-
 button.addEventListener("click", () => {
-  const receivedTag = document.querySelector(".input").value;
+  const receivedTag = document.querySelector(".main-input").value;
+
   const newComponent = new Component(bigDiv, "", receivedTag);
 });
