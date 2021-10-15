5ad7aeea246f2dcb88215353ef75db08eb734f44
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-15 11:43:53 +0200

    Added all the buttons on component file

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import Component from "./Component.js";
 
-const button = document.querySelector(".button");
-const text = document.querySelector(".button").value;
-const div = document.querySelector("div");
+const elementImage = document.querySelector(".container-img");
+const elementButon = document.querySelector(".container-button");
+const elementP = document.querySelector(".container-p");
+const elementInput = document.querySelector(".container-input");
 
-button.addEventListener("click", () => {
-  const receivedTag = document.querySelector(".main-input").value;
-  const newComponent = new Component(bigDiv, "", receivedTag);
+buttonImage.addEventListener("click", () => {
+  const elementImage = new Component(parentElement, "container", "img");
+  const text = document.querySelector(".input-img").value;
+  elementImage.addImage(text);
+});
+
+buttonButton.addEventListener("click", () => {
+  const elementButton = new Component(parentElement, "container", "button");
+  const text = document.querySelector(".input-button").value;
+  elementImage.addButton(text);
 });
