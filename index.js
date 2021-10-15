b87783360ff6db9c746ba1e6aafdb99a8aee46ca
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-15 10:58:27 +0200

    Added .js on import component at index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Component from "./Component";
+import Component from "./Component.js";
 
 const button = document.querySelector(".button");
 const text = document.querySelector(".button").value;
